c345ef9e71e3d12465cec8adc8f505b974d5dc61
idrees ali <studyidrees53@gmail.com>
2024-04-19 09:49:56 +0000

    Automated AWS Project With Terraform

---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = "ap-south-1"  #give your region information here   
+  access_key = ""   #create IAM user and add his access key here 
+  secret_key = ""   #add secret key of the IAM user 
+
+}
