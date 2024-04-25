8556ac9c65295dfc6662f547735077b59b7f092e
idrees ali <studyidrees53@gmail.com>
2024-04-25 10:02:21 +0000

    new s3 static website content

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""    ##Give your access key here 
+  secret_key = ""    ##Give your secret key here 
+}
+
+
